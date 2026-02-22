6b61747565b017472aad93839f28ead78d15f348
Equipo de fundamentos de la programación <joss.rosas.salazar@gmail.com>
2026-02-21 19:00:55 -0600

    Update ej4.c

--- ejercicios/ej4.c
+++ ejercicios/ej4.c
@@ -1,6 +1,9 @@
+Tamano del tipo: Entre a[0] y a[3] hay 3 elementos de distancia. La diferencia en bytes es 5012 - 5000 = 12. Por lo tanto, el tamano de cada elemento es 12 / 3 = 4 bytes.
 
-#include <stdio.h>
-int main(){
-    // Ejercicio 4
-    return 0;
-}
+
+
+Direccion de a[5]: Aplicando la distancia desde la base:
+Dir(a[5]) = 5000 + (5 * 4) = 5020
+
+
+Formula usada: Dir(a[i]) = 5000 + (i * 4)
