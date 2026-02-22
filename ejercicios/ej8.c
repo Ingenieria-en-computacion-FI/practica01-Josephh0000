57f056435c78d9d0f5804c13dba2305437b07eb8
Equipo de fundamentos de la programación <joss.rosas.salazar@gmail.com>
2026-02-21 19:04:18 -0600

    Update ej8.c

--- ejercicios/ej8.c
+++ ejercicios/ej8.c
@@ -1,6 +1,9 @@
+Numero de columnas (C): Sustituimos en la formula de arreglos 2D con los datos base=2000 y m[1][2]=2024:
+2024 = 2000 + ((1 * C + 2) * 4)
+24 = (C + 2) * 4
+6 = C + 2
+C = 4 columnas
++2
 
-#include <stdio.h>
-int main(){
-    // Ejercicio 8
-    return 0;
-}
+
+Tamano total: Si asumimos que es una matriz m[2][4], el calculo seria: 2 filas * 4 columnas * 4 bytes = 32 bytes en total.
