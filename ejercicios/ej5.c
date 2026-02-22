929afc38b55f5131a163fface75c52a14a3ae3ee
Equipo de fundamentos de la programación <joss.rosas.salazar@gmail.com>
2026-02-21 19:03:21 -0600

    Update ej5.c

--- ejercicios/ej5.c
+++ ejercicios/ej5.c
@@ -1,6 +1,6 @@
+Analisis: La afirmacion del estudiante (Si a[0]=1000 entonces a[10]=1010) es incorrecta.
 
-#include <stdio.h>
-int main(){
-    // Ejercicio 5
-    return 0;
-}
+Demostracion Formal: La direccion se calcula con la formula Dir(a[i]) = Base + (i * tamano). Si asumimos un arreglo de enteros (int) de 4 bytes, el calculo real seria:
+Dir(a[10]) = 1000 + (10 * 4) = 1040
+
+Error: El estudiante sumo erroneamente el indice directo a la direccion de memoria sin multiplicarlo por el tamano del tipo de dato.
