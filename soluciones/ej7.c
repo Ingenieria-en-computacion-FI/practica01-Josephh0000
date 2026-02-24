4590b33da29c916b06952452e39d3575be1a9443
Equipo de fundamentos de la programación <joss.rosas.salazar@gmail.com>
2026-02-24 10:19:55 -0600

    Update ej7.c

--- soluciones/ej7.c
+++ soluciones/ej7.c
@@ -1,6 +1,5 @@
+Respuesta: La direccion &m[1][0] es mayor que &m[0][3].
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 7
-    return 0;
-}
+
+Justificacion Matematica: Dado que la matriz "m" tiene 4 columnas, el elemento m[0][3] es el ultimo de la primera fila. Debido al almacenamiento contiguo por filas, el siguiente elemento en la memoria inmediatamente despues es el primer elemento de la siguiente fila, es decir, m[1][0].
+Dir(m[1][0]) = Dir(m[0][3]) + sizeof(int)
