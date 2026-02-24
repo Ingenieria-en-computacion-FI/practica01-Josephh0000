8711043f3fdbe1ed3e0485975f613833e765cff8
Equipo de fundamentos de la programación <joss.rosas.salazar@gmail.com>
2026-02-24 10:18:55 -0600

    Update ej6.c

--- soluciones/ej6.c
+++ soluciones/ej6.c
@@ -1,6 +1,20 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 6
+
+int main() {
+    int m[2][4];
+
+    printf("--- Ejercicio 6 ---\n");
+    for(int i=0; i<2; i++) {
+        for(int j=0; j<4; j++) {
+            printf("Direccion m[%d][%d]: %p\n", i, j, (void*)&m[i][j]);
+        }
+    }
+
+
+    printf("\nConclusion: El almacenamiento es por FILAS.\n");
+    printf("Las direcciones son contiguas: m[0][3] esta justo antes de m[1][0].\n");
+    
+    printf("Formula deducida: Base + ((fila * num_cols) + col) * sizeof(int)\n");
+
     return 0;
 }
